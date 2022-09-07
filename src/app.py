f5c3b4a2b5b96b83d29c6cc0dfb54532b5b2710d
Julian Lancheros <47438320+jlancherosnn@users.noreply.github.com>
2022-09-07 12:21:49 -0500

    .

--- src/app.py
+++ src/app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    print('index')
+    return render_template('index.html')
 
 if __name__ == '__main__':
 
