eefe0b418ab539dc3bbdf229f75f1e0d05c8a58f
jdaneris <70027623+jdaneris@users.noreply.github.com>
2022-11-16 11:11:34 -0500

    librerias

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Documentos\PRACTICAS PYTHON\red\env
+set VIRTUAL_ENV=C:\Users\Alexis\Documents\GitHub\red_social\env
 
 if not defined PROMPT set PROMPT=$P$G
 
