e9ce83b908ec6768bce635443176b157f01e10a1
D3DSYTORR3S <47533997+D3DSYTORR3S@users.noreply.github.com>
2022-11-16 11:58:17 -0500

    app

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from config import *
-from publicaciones import *
+from publicaciones import Publicaciones
+from persona import Persona
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
@@ -11,13 +12,51 @@ con_bd = Conexion()
 @app.route('/')
 def index():
     print('index')
-    return render_template('login/index.html')
-
+    personas = con_bd['Personas']
+    PersonasRegistradas = personas.find()
+    return render_template('login/index.html', personas = PersonasRegistradas)
 
 @app.route('/registro')
 def registro():
-    print('registro')
     return render_template('login/registro.html')
+
+@app.route('/registros', methods=['POST'])
+def registros():
+    print('registro')
+    personas = con_bd['Personas']
+    nombre   = request.form['nombre']
+    apellido = request.form['apellido']
+    nacimiento = request.form['nacimiento']
+    usuario = request.form['usuario']
+    contraseña = request.form['contraseña']
+    correo = request.form['correo']
+    carrera = request.form['carrera']
+    
+    if nombre and apellido and nacimiento and usuario and contraseña and correo and carrera:
+        persona = Persona(nombre, apellido, nacimiento, usuario, contraseña, correo,carrera)
+        personas.insert_one(persona.formato_doc())
+        return render_template('login/index.html')
+    else:
+        return "error"
+    
+@app.route('/validar', methods=['POST'])
+def validaruser():
+    publicacio =con_bd['Personas']
+    usuario =request.form['usuario']
+    password =request.form['password']
+    if usuario and password:
+        person =publicacio.find_one({"usuario":usuario, "contraseña":password})
+        print(person)
+        if person:
+            return redirect(url_for('menu'))
+        else:
+            return redirect(url_for('index'))
+        #publicaciones = Publicaciones(publicacion)
+        #publicacio.insert_one(publicaciones.format_doc())
+        #publicacionesRealizadas =publicacio.find()
+        #return render_template('muro/muroinicio.html', personas = publicacionesRealizadas)
+    else:
+        return"ERROR"
 
 @app.route('/muro')
 def menu():
@@ -60,45 +99,6 @@ def editar_persona(persona_id):
 @app.route('/perfil')
 def perfil():
     return render_template('perfil.html')
-
-##GUARDAR USERS ###################
-
-@app.route('/guaradar_personas', methods=['POST'])
-def agregarUser():
-    user =con_bd['Personas']
-    nombre =request.form['nombre']
-    apellido =request.form['apellido']
-    nacimiento =request.form['nacimiento']
-    usuario =request.form['usuario']
-    contraseña =request.form['contraseña']
-    correo =request.form['correo']
-    carrera =request.form['carrera']
-    if nombre and apellido and nacimiento and usuario and contraseña and correo and carrera :
-        persona = Personas(nombre,apellido,nacimiento,usuario,contraseña,correo,carrera)
-        user.insert_one(persona.format_doc())
-        return render_template('login/index.html')
-    else:
-        return"ERROR"
-
-@app.route('/validar', methods=['POST'])
-def validaruser():
-    publicacio =con_bd['Personas']
-    usuario =request.form['usuario']
-    password =request.form['password']
-    if usuario and password:
-        person =publicacio.find_one({"usuario":usuario, "contraseña":password})
-        print(person)
-        if person:
-            return redirect(url_for('menu'))
-        else:
-            return redirect(url_for('index'))
-        #publicaciones = Publicaciones(publicacion)
-        #publicacio.insert_one(publicaciones.format_doc())
-        #publicacionesRealizadas =publicacio.find()
-        #return render_template('muro/muroinicio.html', personas = publicacionesRealizadas)
-    else:
-        return"ERROR"
-
     
 
 if __name__ == '__main__':
